e39c7a97f5fa505e807061b4de281d94c23823f6
Haider Irfan <haiderirfan62batth@gmail.com>
2023-11-09 03:26:49 +0500

    Avatar Page, NavBar, sidebarNprofile complete v1

---
+++ lib/config/strings.dart
@@ -0,0 +1,7 @@
+//Page Names
+String PageNameAvatar = 'avatar';
+String PageNameLessons = 'lessons';
+String PageNameGestureRecognition = 'gestureRecognition';
+
+//AvatarPage
+String AvatarPageSearch = "TYPE SOMETHING";
--- lib/screens/AvatarPage.dart
+++ lib/screens/AvatarPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sign_saathi_app/config/strings.dart';
+import 'package:sign_saathi_app/utils/NavBar.dart';
+import 'package:sign_saathi_app/utils/sidebarNprofile.dart';
 
 class AvatarPage extends StatefulWidget {
   const AvatarPage({Key? key}) : super(key: key);
@@ -15,69 +18,61 @@ class _AvatarPageState extends State<AvatarPage> {
     return Scaffold(
       backgroundColor: Color(0xFFF6F4EB),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(screenSize.height * 0.02),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.blue, // Blue background circle
-                    child: Icon(Icons.menu, size: screenSize.height * 0.035, color: Colors.white), // White icon
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.blue, // Blue background circle
-                    radius: screenSize.height * 0.04,
-                    child: CircleAvatar(
-                      radius: screenSize.height * 0.035,
-                      backgroundImage: AssetImage('lib/assets/avatar.png'),
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SideBarNProfile(
+                    screenSize: screenSize, imagePath: 'lib/assets/avatar.png'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'lib/assets/character.png',
+                      height: screenSize.height * 0.5,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'lib/assets/character.png',
-                    height: screenSize.height * 0.5,
-                  ),
-                  SizedBox(height: screenSize.height * 0.03),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: BorderSide.none,
+                    SizedBox(height: screenSize.height * 0.06),
+                    Container(
+                      width: screenSize.width * 0.95,
+                      height: screenSize.height * 0.08,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenSize.width * 0.01,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF176B87),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            suffixIcon: IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.send,
+                                  color: Color(0xFF176B87),
+                                )),
+                            hintText: AvatarPageSearch,
+                            hintStyle: TextStyle(
+                                color: Color(0xFF357C93),
+                                fontWeight: FontWeight.w900),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: screenSize.width * 0.05),
+                          ),
                         ),
-                        hintText: 'Type something',
-                        contentPadding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.blue, // Blue background for the bottom icons
-              padding: EdgeInsets.symmetric(vertical: screenSize.height * 0.015),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.person, size: screenSize.height * 0.035, color: Colors.white),
-                  Icon(Icons.camera_alt, size: screenSize.height * 0.035, color: Colors.white),
-                  Icon(Icons.thumb_up, size: screenSize.height * 0.035, color: Colors.white),
-                ],
-              ),
-            ),
-          ],
+                  ],
+                ),
+                SizedBox(height: screenSize.height * 0.10),
+                NavBar(screenSize: screenSize, pageName: PageNameAvatar),
+              ]),
         ),
       ),
     );
